7b4dc1e606c119e493da3eeff1f7f1ad06b8b7ef
Sanaz Sadat Golriz <Sanaz@compassdigital.io>
2017-11-17 15:44:30 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,13 +1,11 @@
 # load necessary libraries
 library(dplyr)
 
-data <- read.table("../Downloads/Coursera/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-df_filter <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+df <- read.table("../Downloads/Coursera/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+df_filter <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
 
 # first plot 
-
-#str(subSetData)
-globalActivePower <- as.numeric(df_filter$Global_active_power)
+gAP <- as.numeric(df_filter$Global_active_power)
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(gAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
